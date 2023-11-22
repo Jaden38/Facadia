07a0479e462fabbe36eec352926c3c0dc4d4e03b
Jaden38 <damien.nithard@gmail.com>
2023-11-22 12:36:52 +0100

    Unit test pagination

---
+++ js/pages/common/pagination/index.test.js
@@ -0,0 +1,18 @@
+import Pagination from './index';
+/**
+ * @function Pagination.getNumberOfPages
+ */
+describe('Pagination Unit Test Suites', () => {
+    it('should be defined', () => {
+        expect(Pagination.getNumberOfPages(12)).toBeDefined();
+    })
+    it('should return 0', () => {
+        expect(Pagination.getNumberOfPages(0)).toBe(0);
+    })
+    it('should return 1', () => {
+        expect(Pagination.getNumberOfPages(7)).toBe(1);
+    })
+    it('should return 5', () => {
+        expect(Pagination.getNumberOfPages(34)).toBe(5);
+    })
+})
