cc927c1d107559c8c66ae37574d0681e1bf30b8c
Jaden38 <damien.nithard@gmail.com>
2023-11-22 16:04:31 +0100

    Premier unit test router

--- js/router/index.test.js
+++ js/router/index.test.js
@@ -3,13 +3,14 @@
 */
 
 import { getByTestId } from '@testing-library/dom';
-import {router} from './index.js';
+import { router } from './index.js';
+import '@testing-library/jest-dom/extend-expect';
 
 describe('Visiting the / URL', () => {
     it('Should render the login page with the title "Veuillez vous connecter"', async () => {
-        document.location='/';
-        document.body.innerHTML='<div id="root"></div>';
+        document.location = '/';
+        document.body.innerHTML = '<div id="root"></div>';
         await router();
-        expect(getByTestId(document.body, 'sign-in-form-title').textContent).toHaveTextContent('Veuillez vous connecter');
+        expect(getByTestId(document.body, 'sign-in-form-title')).toHaveTextContent('Veuillez vous connecter');
     });
 });
